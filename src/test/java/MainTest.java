af17f3135234fbb9d2af4e1b564e66280a666beb
yuyang <yuyangxxxmail@gmail.com>
2024-04-03 14:32:32 +0800

    add files

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,11 @@
+import  org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+
+
+public class MainTest {
+    @Test
+    public void myTestMethod() {
+        Assertions.assertEquals(1 ,1  );
+    }
+}
